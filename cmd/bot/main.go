be569b58831f9c17e704359035f780ddad08446b
leveebreaks <83450119+leveebreaks@users.noreply.github.com>
2021-11-24 00:22:49 +0200

    go mod updated

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"fmt"
 	"github.com/leveebreaks/fobattle/config"
+	tb "github.com/tucnak/telebot"
+	"time"
 )
 
 func main() {
 	var env config.Env = config.DEV
 
 	conf := config.Build(env)
+	b, err := tb.NewBot(tb.Settings{
+		Token:  conf.Token,
+		Poller: &tb.LongPoller{Timeout: 2 * time.Second},
+	})
+
+	if err != nil {
+		panic(err)
+	}
+	b.Start()
 
 	fmt.Println(conf)
-
 }
--- go.mod
+++ go.mod
@@ -6,5 +6,4 @@ require (
 	github.com/mitchellh/hashstructure v1.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/tucnak/telebot v2.0.0+incompatible // indirect
-	gopkg.in/tucnak/telebot.v2 v2.4.1 // indirect
 )
